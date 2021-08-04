d7aa302a9354edd8d903b06a59b40792424d4118
JonathanGzzBen <jonathangzzben@gmail.com>
2021-08-03 21:25:42 -0500

    Replace command with slash command

--- duff/main.py
+++ duff/main.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
 import discord
-from discord import embeds
-from discord.channel import TextChannel
+from discord_slash import SlashCommand, SlashContext
 from dotenv import dotenv_values
 from mal.anime import animes_season
 config = dotenv_values(".env")
 
-class DuffClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-    
-    async def on_message(self, message: discord.Message):
-        if message.author == client.user:
-            return
-        if str(message.content).startswith("animes de la season"):
-            await self.reply_animes_season(message)
-    
-    async def reply_animes_season(self, message: discord.Message):
-        await message.reply("Mostrando los 5 animes con mas miembros en MyAnimeList")
-        for anime in animes_season(limit=5):
-            embed_message = discord.Embed()
-            embed_message.title = anime.title
-            embed_message = (embed_message
-                .add_field(name="Score", value=anime.score)
-                .add_field(name="Episodes", value=anime.episodes)
-                .add_field(name="Studio", value=anime.studio)
-                .set_image(url=anime.image_url))
+client = discord.Client(intents=discord.Intents.default())
+slash = SlashCommand(client, sync_commands=True)
 
-            await message.channel.send(embed=embed_message)
-    
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
 
-client = DuffClient()
+@slash.slash(name="season")
+async def reply_animes_season(ctx:SlashContext, limit:int = 5):
+    await ctx.reply("Mostrando los {} animes con mas miembros en MyAnimeList".format(limit))
+    for anime in animes_season(limit):
+        embed_message = discord.Embed()
+        embed_message.title = anime.title
+        embed_message = (embed_message
+            .add_field(name="Score", value=anime.score)
+            .add_field(name="Episodes", value=anime.episodes)
+            .add_field(name="Studio", value=anime.studio)
+            .set_image(url=anime.image_url))
+
+        await ctx.channel.send(embed=embed_message)
+
 client.run(config["DISCORD_TOKEN"])
