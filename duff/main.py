79cdb3403b893ffb7ac780379d3e1c314b91de2e
JonathanGzzBen <jonathangzzben@gmail.com>
2021-08-04 10:40:15 -0500

    Add search anime command

--- duff/main.py
+++ duff/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import discord
 from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
 from dotenv import dotenv_values
-from mal.anime import animes_season
-config = dotenv_values(".env")
+from mal.anime import animes_season, search_anime
 
+config = dotenv_values(".env")
 client = discord.Client(intents=discord.Intents.default())
 slash = SlashCommand(client, sync_commands=True)
 
@@ -26,5 +27,22 @@ async def reply_animes_season(ctx:SlashContext, limit:int = 5):
             .set_image(url=anime.image_url))
 
         await ctx.channel.send(embed=embed_message)
+
+@slash.slash(name="anime")
+async def reply_broadcast(ctx:SlashContext, anime:str):
+    anime_info = search_anime(anime)
+    if anime_info == None:
+        await ctx.reply("No pude encontrar informacion de ese anime. uwu")
+        return
+    embed_message = discord.Embed()
+    embed_message.title = anime_info.title
+    embed_message = (embed_message
+        .add_field(name="Score", value=anime_info.score)
+        .add_field(name="Studio", value=anime_info.studio)
+        .add_field(name="Episodes", value=anime_info.episodes)
+        .add_field(name="Broadcast", value=anime_info.broadcast)
+        .set_image(url=anime_info.image_url)
+        )
+    await ctx.reply(embed=embed_message)
 
 client.run(config["DISCORD_TOKEN"])
--- duff/mal/anime.py
+++ duff/mal/anime.py
@@ -1,7 +1,7 @@
 from typing import List
 from urllib.request import urlopen
+import urllib.parse
 from bs4 import BeautifulSoup
-import operator
 
 
 class Anime:
@@ -10,6 +10,7 @@ class Anime:
     studio: str
     episodes: str
     image_url: str
+    broadcast: str
 
 
 def animes_season(limit: int = 0) -> List[Anime]:
@@ -44,6 +45,37 @@ def animes_season(limit: int = 0) -> List[Anime]:
     except:
         return []
 
+def search_anime(anime_search: str) -> Anime:
+    try:
+        anime_search_url = ("https://myanimelist.net/anime.php?q={0}&cat=anime"
+            .format(urllib.parse.quote(anime_search)))
+        anime_search_html = urlopen(anime_search_url)
+        bsAnimeSearchPage = BeautifulSoup(anime_search_html.read(), 'html.parser')
+        anime_url = bsAnimeSearchPage.find("div", {"class": "js-categories-seasonal"}).table.find_all("tr")[1].a["href"]
+        anime_page_html = urlopen(anime_url)
+        bsAnimePage = BeautifulSoup(anime_page_html.read(), "html.parser")
+        anime = Anime()
+        anime.title = bsAnimePage.find("h1", {"class": "title-name"}).get_text().strip()
+        anime.score = bsAnimePage.find("div", {"class": "score-label"}).get_text().strip()
+        anime.image_url = bsAnimePage.find("td", {"class": "borderClass"}).div.div.a.img["data-src"].strip()
+        infos_headers = bsAnimePage.find_all("span", {"class": "dark_text"})
+        for header in infos_headers:
+            header_text = header.get_text().strip()
+            if header_text == "Episodes:":
+                episodes_container = header.parent
+                header.extract()
+                anime.episodes = episodes_container.get_text().strip()
+            elif header_text == "Broadcast:":
+                broadcast_container = header.parent
+                header.extract()
+                anime.broadcast = broadcast_container.get_text().strip()
+            elif header_text == "Studios:":
+                studios_container = header.parent
+                header.extract()
+                anime.studio = studios_container.get_text().strip()
+        return anime
+    except:
+        return None
 
 if __name__ == "__main__":
     seasonal_animes = animes_season(limit=5)
